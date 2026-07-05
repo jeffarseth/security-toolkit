4fd34376646b09d389052627ba2468596f454377
jeffarseth <jeffarsethbuenaflor.atiagan@gmail.com>
2026-07-04 21:32:01 -0500

    Add simple hash comparison tool

---
+++ hash_tool/compare.py
@@ -0,0 +1,35 @@
+# Jeffar - Hash Compare
+# Description - Compares two known hashes the user provides and check if it's a match.
+# Created - 2026-07-04
+# Last Updated - 2026-07-04
+
+# Main function
+def main():
+    hash1 = ""          # hash to compare from
+    hash2 = ""          # hash to compare to
+    ignore_case = ""    # option to ignore case-sensitivity
+    match = False       # tells if the two hashes are a match: initialized to False
+
+    print("\nHASH COMPARE")
+
+    hash1 = input("Paste hash 1: ").strip()
+    hash2 = input("Paste hash 2: ").strip()
+    ignore_case = input("Ignore case? (Y/N): ").strip().upper()
+
+    while True:
+        ignore_case = input("Ignore case? (Y/N): ").strip().upper()
+
+        if ignore_case == 'Y':
+            match = hash1.lower() == hash2.lower()
+            break
+        elif ignore_case == 'N':
+            match = hash1 == hash2
+            break
+        else:
+            print("\033[31mINVALID INPUT\033[0m")
+
+    print("MATCH" if match else "NO MATCH")
+
+# dunder name guard
+if __name__ == "__main__":
+    main()
