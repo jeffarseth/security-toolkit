e5d8b9c1ce864899adfec8b73e6b7e029bb3c204
jeffarseth <jeffarsethbuenaflor.atiagan@gmail.com>
2026-07-11 17:27:16 -0500

    Add export to txt/json/csv

--- port_scanner/scanner.py
+++ port_scanner/scanner.py
@@ -1,7 +1,7 @@
 # Jeffar - Port Scanner
 # Description - Scans a target's ports with multithreading, service detection, and a summary report.
 # Created - 2026-07-05
-# Last updated - 2026-07-10
+# Last updated - 2026-07-11
 
 # IMPORTANT NOTE!!
 # SCOPE: only scan localhost or hosts you are explicitly authorized to test.
@@ -9,81 +9,45 @@
 # Modules
 import port_scanner.services as services            # for ports service lookup
 from concurrent.futures import ThreadPoolExecutor   # for multithreading
+from pathlib import Path                            # for filepath
 import time                                         # for elapsed time
 import socket                                       # for sock: networking module
+import json                                         # for exporting to json
+import csv                                          # for exporting to csv
 
 # Constants
-BUFFER_SIZE = 1024                          # maximum number of bytes to read in one call.
-THREAD_LIMIT = 2000                         # maximum amount of threads to use
+BUFFER_SIZE = 1024      # maximum number of bytes to read in one call.
+THREAD_LIMIT = 2000     # maximum amount of threads to use
 
 def main():
-    user_input = ""         # blank user input in str
-    ip = ""                 # target ip
-    ports = []              # target port/s
-    timeout = 1             # how long to wait (in seconds) for a response before giving up on this port: initialized to 1 second for default
-    start_time = 0          # start time for recording elapsed time during scan: initialized to 0 seconds
-    end_time = 0            # end time for recording elapsed time during scan: initialized to 0 seconds
-    results = []            # holds port number, service name, and banner
-    open_ports = 0          # amount of open ports: initialized to 0
-    max_workers = 200       # amount of threads. how many ports to check concurrently: initialized to 200 threads for default
+    ip = ""             # target ip
+    ports = []          # target port/s
+    timeout = 0         # how long to wait (in seconds) for a response before giving up on this port
+    max_workers = 0     # amount of threads. how many ports to check concurrently
+    start_time = 0      # start time for recording elapsed time during scan: initialized to 0 seconds
+    end_time = 0        # end time for recording elapsed time during scan: initialized to 0 seconds
+    results = []        # holds port number, service name, and banner
+    open_ports = 0      # amount of open ports: initialized to 0
 
     print("\nPORT SCANNER")
+
     ip = get_target()
     ports = get_ports()
+    timeout = get_timeout()
+    max_workers = get_threads()
 
-    # input validation
-    while True:
-        try:
-            user_input = input("\nTimeout in seconds (leave blank for 1s default): ").strip()
-
-            if user_input == "":                                # default if input is blank
-                break
-
-            timeout = float(user_input)                         # convert str to float
-
-            if timeout > 0:                                     # timeout has to be greater than 0 seconds
-                break
-            else:
-                print("\033[31mINVALID INPUT\033[0m")
-
-        except ValueError:
-            print("\033[31mINVALID INPUT - enter a valid number!\033[0m")
-
-    # input validation
-    while True:
-        try:
-            user_input = input("Max concurrent threads (leave blank for 200 default): ").strip()
-
-            if user_input == "":                                # default if input is blank
-                break
-
-            max_workers = int(user_input)                       # convert str to int
-
-            if 0 < max_workers <= THREAD_LIMIT:                 # threads has to greater than 0
-                break
-            elif max_workers > THREAD_LIMIT:                    # thread amount: safety net for system stability
-                print(f"\033[31mCAPPED AT {THREAD_LIMIT} - enter a number <= {THREAD_LIMIT}\033[0m")
-            else:
-                print("\033[31mINVALID INPUT\033[0m")
-
-        except ValueError:
-            print("\033[31mINVALID INPUT - enter a whole number!\033[0m")
-
-    start_time = time.time()
+    start_time = time.time()        # start recording time
     results = scan_ipv4_tcp(ip, ports, timeout, max_workers)
     open_ports = len(results)
-    end_time = time.time()
+    end_time = time.time()          # stop recording time (ザワールド)
 
-    print("\nScan complete.")
-    print(f"Ports scanned: {len(ports)}")
-    print(f"Open: {open_ports}")
-    print(f"Closed: {len(ports) - open_ports}")
-    print(f"Time taken: {end_time - start_time:.2f} seconds")   # rounded to hundredths
+    export(results)
+    print_summary(ports, open_ports, start_time, end_time)
 
 # Functions
 def get_target():
     """
-    get_target - prompt user for target (hostname or IP)
+    get_target - prompts the user for target (hostname or IP)
     """
 
     target = ""     # target to scan
@@ -102,17 +66,17 @@ def get_target():
         
 def get_ports():
     """
-    get_ports - prompts user for scan mode (quick/full/customr/customs) and returns port collection to scan
+    get_ports - prompts the user for scan mode (quick/full/customr/customs) and returns port collection to scan
     """
 
     user_input = ""
     lowest = ""         # for custom ports (lowest to highest)
     highest = ""        # for custom ports (lowest to highest)
 
+    port = None                                     # placeholder for adding ports in port selection
     quick_ports = list(services.SERVICE_PORTS)      # list of common port numbers from SERVICE_PORTS
     full_ports = range(0, 65536)                    # list of port numbers from 0-65535
     custom_ports = []                               # list of ports from user's choice
-    port = None                                     # placeholder for adding ports in port selection
 
     # input validation
     while True:
@@ -167,6 +131,10 @@ def get_ports():
                     if 0 <= port < 65536:                                               # user gets to select a port to scan (0-65535)
                         if port not in custom_ports:                                    # check if port is in custom_ports already (duplicate checking)
                             custom_ports.append(port)                                   # add port to list
+
+                            if len(custom_ports) == 65536:                              # all ports selected
+                                return custom_ports
+
                         else:
                             print("\033[31mPORT ALREADY ADDED\033[0m")
                             continue
@@ -178,17 +146,69 @@ def get_ports():
                     continue
 
                 while True:
-                    user_input = input("Enter another port (Y/N)? ").strip().upper()    # prompt user for another port to enter
+                    user_input = input("Enter another port (Y/n)? ").strip().upper()    # prompt user for another port to enter
 
                     if user_input == "N":
                         return custom_ports
-                    elif user_input == "Y":                                             # repeat until there are no more ports to scan
+                    elif user_input == "Y" or user_input == "":                         # repeat until there are no more ports to scan
                         break
                     else:
                         print("\033[31mINVALID INPUT\033[0m")
         
         else:
             print("\033[31mINVALID INPUT\033[0m")
+
+def get_timeout():
+    """
+    get_timeout - prompts the user for timeout
+    """
+
+    timeout = 1     # how long to wait (in seconds) for a response before giving up on this port: initialized to 1 second for default
+
+    # input validation
+    while True:
+        try:
+            user_input = input("\nTimeout in seconds (leave blank for 1s default): ").strip()
+
+            if user_input == "":                                # default if input is blank
+                return timeout
+
+            timeout = float(user_input)                         # convert str to float
+
+            if timeout > 0:                                     # timeout has to be greater than 0 seconds
+                return timeout
+            else:
+                print("\033[31mINVALID INPUT\033[0m")
+
+        except ValueError:
+            print("\033[31mINVALID INPUT - enter a valid number!\033[0m")
+
+def get_threads():
+    """
+    get_threads - prompts the user for amount of max_workers (threads)
+    """
+
+    max_workers = 200   # amount of threads. how many ports to check concurrently: initialized to 200 threads for default
+
+    # input validation
+    while True:
+        try:
+            user_input = input("Max concurrent threads (leave blank for 200 default): ").strip()
+
+            if user_input == "":                                # default if input is blank
+                break
+
+            max_workers = int(user_input)                       # convert str to int
+
+            if 0 < max_workers <= THREAD_LIMIT:                 # threads has to greater than 0
+                break
+            elif max_workers > THREAD_LIMIT:                    # thread amount: safety net for system stability
+                print(f"\033[31mCAPPED AT {THREAD_LIMIT} - enter a number <= {THREAD_LIMIT}\033[0m")
+            else:
+                print("\033[31mINVALID INPUT\033[0m")
+
+        except ValueError:
+            print("\033[31mINVALID INPUT - enter a whole number!\033[0m")
 
 def scan_one_port(ip, port, timeout):
     """
@@ -225,7 +245,7 @@ def scan_one_port(ip, port, timeout):
         service_name = services.get_service(port)
 
         # print port number, service name if in dictionary, and banner if it exists
-        print(f"PORT {port}     OPEN    ({service_name})   {banner if banner else 'no banner'}")
+        print(f"\nPORT {port}    OPEN    ({service_name})    {banner if banner else 'no banner'}", end="")
 
         # append to result[]
         result = {"port": port, "service": service_name, "banner": banner}
@@ -250,7 +270,7 @@ def scan_ipv4_tcp(ip, ports, timeout, max_workers):
 
     results = []                                                        # holds result[]
 
-    print(f"\nScanning {len(ports)} ports on {ip}...\n")
+    print(f"\nScanning {len(ports)} ports on {ip}...")
 
     with ThreadPoolExecutor(max_workers=max_workers) as executor:       # user-controlled concurrency, capped at THREAD_LIMIT for system stability
         
@@ -261,6 +281,125 @@ def scan_ipv4_tcp(ip, ports, timeout, max_workers):
         results = [r for r in all_results if r is not None]
 
     return results
+
+def export(results):
+    """
+    export - prompts the user save the results to a file (plain text, JSON, or CSV) in a folder of their choosing
+
+    results - list of result dicts (port, service, banner)
+
+    filepath - where to save the file
+    """
+
+    # function lookup table for exporting to a file
+    export_methods = {
+        "txt": export_txt,
+        "json": export_json,
+        "csv": export_csv
+    }
+
+    user_input = ""
+    filepath = ""           # where to save file (dir)
+    selected_format = ""    # choose to save as txt, json, or csv
+    filename = "port_scan_results"  # saved file name: default to "port_scan"
+    savepath = ""           # saved file with dir and extension
+
+    # input validation
+    while True:
+        user_input = input("\nExport results (y/N)? ").strip().upper()
+
+        if user_input == "N" or user_input == "":
+            return
+        elif user_input == "Y":
+            break
+        else:
+            print("\033[31mINVALID INPUT\033[0m")
+
+    # input validation
+    while True:
+        user_input = input("Enter filepath: ")    # get raw string first
+        filepath = Path(user_input.strip().strip('"').strip("'"))       # removes leading/trailing whitespace/newlines and surrounding Windows quotes when using "Copy as path"
+        filepath = filepath.resolve()                                   # turn it into an absolute path
+        
+        if filepath.exists() and filepath.is_file():                    # path exists but it's a file
+            print("\033[31mPath is a file, not a folder\033[0m")
+        elif filepath.exists() and filepath.is_dir():                   # path exists and it's a folder
+            break
+        else:
+            filepath.mkdir(parents=True, exist_ok=True)                 # make a directory, include parents on a nested dir, and ignore "Folder already there" warnings
+            break
+
+    
+    user_input = input("Enter filename: ").strip()                      # prompt user for file's name
+    if user_input != "":                                                # use user_input if not left default
+        filename = user_input
+
+    # input validation
+    while True:
+        selected_format = input("Format (txt/json/csv): ").strip().lower()
+        
+        if selected_format in export_methods:                           # only use function if it's a valid exporting method
+            savepath = filepath / f"{filename}.{selected_format}"       # combine folder, filename, extension
+            export_methods[selected_format](results, savepath)
+            break
+        else:
+            print("\033[31mINVALID INPUT\033[0m")
+        
+def export_txt(results, savepath):
+    """
+    export_txt - writes the results list to a text file
+
+    results - list of result dicts (port, service, banner)
+
+    savepath - saved file with dir and extension
+    """
+
+    with open(savepath, "w", encoding="utf-8") as file:                 # writes to savepath in utf-8 and create object file
+        for result in results:                                          # iterate every result inside results
+            # write every line of open ports
+            file.write(f"PORT {result['port']}    OPEN    ({result['service']})    {result['banner'] if result['banner'] else 'no banner'}\n")
+
+def export_json(results, savepath):
+    """
+    export_json - writes the results list to a JSON file
+
+    results - list of result dicts (port, service, banner)
+
+    savepath - saved file with dir and extension
+    """
+
+    with open(savepath, "w", encoding="utf-8") as file:                 # writes to savepath in utf-8 and create object file
+        json.dump(results, file, indent=4)                              # write results to file with most used tab indentation
+
+def export_csv(results, savepath):
+    """
+    export_csv - writes the results list to a CSV file
+    
+    results - list of result dicts (port, service, banner)
+
+    savepath - saved file with dir and extension
+    """
+
+    with open(savepath, "w", newline="", encoding="utf-8") as file:     # writes to savepath in utf-8 without automatic \n and create object file
+        writer = csv.DictWriter(
+            file,                                                       # write to file object
+            fieldnames=["port", "service", "banner"]                    # CSV column order
+        )
+
+        # write port, service, and banner
+        writer.writeheader()
+        writer.writerows(results)
+
+def print_summary(ports, open_ports, start_time, end_time):
+    """
+    print_summary - prints ports scanned, amount of open ports, amount of closed ports, and the time taken to scan the target
+    """
+
+    print("\nScan complete.")
+    print(f"Ports scanned: {len(ports)}")
+    print(f"Open: {open_ports}")
+    print(f"Closed: {len(ports) - open_ports}")
+    print(f"Time taken: {end_time - start_time:.2f} seconds")   # rounded to hundredths
 
 # dunder name guard
 if __name__ == "__main__":
